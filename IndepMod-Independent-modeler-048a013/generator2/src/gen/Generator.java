eebfcc46ade9a3a61849b3702789e7129cf4552e
Zdeněk <zdenek.dusatko@gmail.com>
2011-04-07 17:30:32 +0200

    GUI panel

--- IndepMod-Independent-modeler-048a013/generator2/src/gen/Generator.java
+++ IndepMod-Independent-modeler-048a013/generator2/src/gen/Generator.java
@@ -9,29 +9,45 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.util.logging.Logger;
+import org.openide.DialogDisplayer;
+import org.openide.NotifyDescriptor;
 import org.openide.util.Exceptions;
 import org.openide.util.Utilities;
 
 public final class Generator implements ActionListener {
+
     private static final Logger LOG = Logger.getLogger(ClassModel.class.getName());
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        IClassModelModel classModel = Utilities.actionsGlobalContext().lookup(IClassModelModel.class);
-        if (classModel == null) {
-            //Oznam uzivateli, ze neni v zadnem class modelu
-            System.out.println("Nemate otevren class model");
-        } else {
-            //Zacni generovat
-            System.out.println("Otevreny classs model ma: "+ classModel.getClasses().size() +" trid.");
-            GenJavaClass gen = new GenJavaClass(classModel);
-            GenSQL genSql = new GenSQL(classModel);
-            try {
-                gen.generateModel();
-                genSql.generateModel();
-            } catch (IOException ex) {
-                Exceptions.printStackTrace(ex);
+        GeneratorDialog panel = new GeneratorDialog();
+        NotifyDescriptor nd = new NotifyDescriptor(panel, "Code Generator", NotifyDescriptor.OK_CANCEL_OPTION,
+                NotifyDescriptor.QUESTION_MESSAGE, null, NotifyDescriptor.OK_OPTION);
+        if (DialogDisplayer.getDefault().notify(nd) == NotifyDescriptor.OK_OPTION) {
+
+
+            IClassModelModel classModel = Utilities.actionsGlobalContext().lookup(IClassModelModel.class);
+            
+            String save_path = panel.getSave_path();
+            String format = (String)panel.getSelectedItem();  
+            
+            if (classModel == null) {
+                //Oznam uzivateli, ze neni v zadnem class modelu
+                System.out.println("Nemate otevren class model");
+            } else {
+                //Zacni generovat
+                System.out.println("Otevreny classs model ma: " + classModel.getClasses().size() + " trid.");
+                GenJavaClass gen = new GenJavaClass(classModel);
+                GenSQL genSql = new GenSQL(classModel);
+                try {
+                    gen.generateModel();
+                    genSql.generateModel();
+                } catch (IOException ex) {
+                    Exceptions.printStackTrace(ex);
+                }
+
             }
+            
             
         }
     }
